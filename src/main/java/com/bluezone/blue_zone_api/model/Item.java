34064361b8cdfdd64c9cc8060c30e1868287fb5c
pedro de oliveira <pe.olivsilv@gmail.com>
2026-05-26 12:17:42 -0300

    atualiza Item

--- src/main/java/com/bluezone/blue_zone_api/model/Item.java
+++ src/main/java/com/bluezone/blue_zone_api/model/Item.java
@@ -1,25 +1,97 @@
 package com.bluezone.blue_zone_api.model;
 
 public class Item {
-    private String nome;
-    private int quantidade;
+    private String categoriaPrincipal;
+    private String subcategoria;
+    private String item;
+    private String controlado;
+    private String quantidade;
+    private String estoqueMinimo;
+    private String dataVencimento;
+    private String status;
+    private String observacoes;
+    private String ultimaAtualizacao;
 
     public Item() {
     }
 
-    public String getNome() {
-        return nome;
+    public String getCategoriaPrincipal() {
+        return categoriaPrincipal;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public void setCategoriaPrincipal(String categoriaPrincipal) {
+        this.categoriaPrincipal = categoriaPrincipal;
     }
 
-    public int getQuantidade() {
+    public String getSubcategoria() {
+        return subcategoria;
+    }
+
+    public void setSubcategoria(String subcategoria) {
+        this.subcategoria = subcategoria;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public String getControlado() {
+        return controlado;
+    }
+
+    public void setControlado(String controlado) {
+        this.controlado = controlado;
+    }
+
+    public String getQuantidade() {
         return quantidade;
     }
 
-    public void setQuantidade(int quantidade) {
+    public void setQuantidade(String quantidade) {
         this.quantidade = quantidade;
+    }
+
+    public String getEstoqueMinimo() {
+        return estoqueMinimo;
+    }
+
+    public void setEstoqueMinimo(String estoqueMinimo) {
+        this.estoqueMinimo = estoqueMinimo;
+    }
+
+    public String getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(String dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public String getUltimaAtualizacao() {
+        return ultimaAtualizacao;
+    }
+
+    public void setUltimaAtualizacao(String ultimaAtualizacao) {
+        this.ultimaAtualizacao = ultimaAtualizacao;
     }
 }
