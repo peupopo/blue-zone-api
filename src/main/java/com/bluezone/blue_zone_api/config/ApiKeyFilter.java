33c5789778d3acb83b0e5dccde15efce052e5b3b
pedro de oliveira <pe.olivsilv@gmail.com>
2026-05-27 12:01:23 -0300

    adiciona autenticação por chave de API

---
+++ src/main/java/com/bluezone/blue_zone_api/config/ApiKeyFilter.java
@@ -0,0 +1,34 @@
+package com.bluezone.blue_zone_api.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyFilter extends OncePerRequestFilter {
+
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String keyRecebida = request.getHeader("X-API-Key");
+
+        if (apiKey.equals(keyRecebida)) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+            response.getWriter().write("API Key inválida");
+        }
+    }
+}
